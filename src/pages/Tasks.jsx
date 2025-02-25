747501fa4104f53f32ec756240a5c75c1646ef28
khadija-urmi <khadija5jahan211@gmail.com>
2025-02-25 23:02:01 +0600

    added handle drag end function

--- src/pages/Tasks.jsx
+++ src/pages/Tasks.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Column from "../components/Column";
+import { DndContext } from "@dnd-kit/core";
 
 const Tasks = () => {
   const INITIAL_TASKS = [
@@ -35,20 +36,40 @@ const Tasks = () => {
   ];
   const [tasks, setTasks] = useState(INITIAL_TASKS);
 
-  const handleDragEndTask = async (event) => {};
+  const handleDragEndTask = async (event) => {
+    const { active, over } = event;
+
+    if (!over) return;
+
+    const taskId = active.id;
+    const newStatus = over.id;
+
+    setTasks(() =>
+      tasks.map((task) =>
+        task.id === taskId
+          ? {
+              ...tasks,
+              status: newStatus,
+            }
+          : task
+      )
+    );
+  };
 
   return (
     <div className="p-4 mt-16">
       <div className="flex gap-8 mt-3">
-        {COLUMNS.map((column) => {
-          return (
-            <Column
-              key={column.id}
-              column={column}
-              tasks={tasks.filter((task) => task.status === column.id)}
-            />
-          );
-        })}
+        <DndContext onDragEnd={handleDragEndTask}>
+          {COLUMNS.map((column) => {
+            return (
+              <Column
+                key={column.id}
+                column={column}
+                tasks={tasks.filter((task) => task.status === column.id)}
+              />
+            );
+          })}
+        </DndContext>
       </div>
     </div>
   );
